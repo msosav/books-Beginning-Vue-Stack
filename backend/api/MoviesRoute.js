3e8db769892078c7aef8e982ce66aa6eaa4dfab5
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2023-12-14 23:11:35 -0500

    imp: Movies search

--- backend/api/MoviesRoute.js
+++ backend/api/MoviesRoute.js
@@ -1,8 +1,8 @@
+import MoviesController from "./controllers/MoviesController.js";
+
 export default class MoviesRoute {
   static configRoutes(router) {
-    router.route("/").get((req, res) => {
-      res.send("Hello World");
-    });
+    router.route("/").get(MoviesController.apiGetMovies);
     return router;
   }
 }
