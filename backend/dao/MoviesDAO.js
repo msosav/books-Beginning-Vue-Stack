2f251ad04662bceb0d4ae2f3597251d4c9d55967
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2023-12-14 22:39:24 -0500

    fix: Missing bracket

--- backend/dao/MoviesDAO.js
+++ backend/dao/MoviesDAO.js
@@ -16,4 +16,32 @@ export default class MoviesDAO {
       );
     }
   }
+
+  static async getMovies({
+    filters = null,
+    page = 0,
+    moviesPerPage = 20,
+  } = {}) {
+    let query;
+    if (filters) {
+      if ("title" in filters) {
+        query = { $text: { $search: filters["title"] } };
+      } else if ("rated" in filters) {
+        query = { rated: { $eq: filters["rated"] } };
+      }
+    }
+
+    let cursor;
+    try {
+      cursor = await MoviesDAO.movies
+        .find(query)
+        .limit(moviesPerPage)
+        .skip(moviesPerPage * page);
+      const moviesList = await cursor.toArray();
+      const totalNumMovies = await MoviesDAO.movies.countDocuments(query);
+    } catch (e) {
+      console.error(`Unable to issue find command, ${e}`);
+      return { moviesList: [], totalNumMovies: 0 };
+    }
+  }
 }
